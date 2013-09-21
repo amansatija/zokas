f104e8cf92b14a4f008f8cc6ba367251625324c6
Baldawa <baldawar@amazon.com>
2013-09-21 03:41:06 -0700

    more clean ups

--- js/zoke.js
+++ js/zoke.js
@@ -1,17 +1,19 @@
 $( document ).ready(function() {
-    subreddit_url = ['http://www.reddit.com/r/oneliners/.json?','http://www.reddit.com/r/oneliners/top.json?t=all&',
-                    'http://www.reddit.com/r/oneliners/top.json?t=month&','http://www.reddit.com/r/oneliners/top.json?t=week&',
-                    'http://www.reddit.com/r/cleanjokes/.json?','http://www.reddit.com/r/cleanjokes/top.json?t=all&',
-                    'http://www.reddit.com/r/cleanjokes/top.json?t=month&','http://www.reddit.com/r/cleanjokes/top.json?t=week&',
-                    'http://www.reddit.com/r/Jokes/.json?','http://www.reddit.com/r/Jokes/top.json?t=all&',
-                    'http://www.reddit.com/r/Jokes/top.json?t=month&','http://www.reddit.com/r/Jokes/top.json?t=week&']
+    subreddit_url = ['http://www.reddit.com/r/oneliners/.json?',
+                    'http://www.reddit.com/r/oneliners/top.json?t=all&',
+                    'http://www.reddit.com/r/oneliners/top.json?t=month&',
+                    'http://www.reddit.com/r/oneliners/top.json?t=week&',
+                    'http://www.reddit.com/r/cleanjokes/.json?',
+                    'http://www.reddit.com/r/cleanjokes/top.json?t=all&',
+                    'http://www.reddit.com/r/cleanjokes/top.json?t=month&',
+                    'http://www.reddit.com/r/cleanjokes/top.json?t=week&',
+                    'http://www.reddit.com/r/Jokes/.json?',
+                    'http://www.reddit.com/r/Jokes/top.json?t=all&',
+                    'http://www.reddit.com/r/Jokes/top.json?t=month&',
+                    'http://www.reddit.com/r/Jokes/top.json?t=week&']
     append = 'limit=100&jsonp=?'
 
-    randomFrom = function(arr) {
-        returnval = arr[Math.floor(Math.random() * arr.length)]
-        console.log(returnval)
-        return returnval
-    }
+    random = function(arr) { return arr[Math.floor(Math.random() * arr.length)] }
 
     getSelfie = function(post) {
         result = ""
@@ -24,26 +26,26 @@ $( document ).ready(function() {
         return result;
     }
 
-    pickShortPost = function(response, counter) {
-        post = randomFrom(response.data.children).data
-        return (counter === 0 || post.selftext.length < 1000)? post : pickShortPost(response, --counter)
+    pickShortPost = function(posts, counter) {
+        post = random(posts).data
+        return (counter === 0 || post.selftext.length < 1000)? post : pickShortPost(posts, --counter)
     }
 
-    getHTMLFrom = function(response) {
-        post = pickShortPost(response, response.data.children.length)
+    pickZoke = function(response) {
+        posts = response.data.children
+        post = pickShortPost(posts, posts.length)
         self = getSelfie(post)
         return "<strong>" + post.title + "</strong>...<br/>" + self
     }
 
-    url = randomFrom(subreddit_url) + append
     $.ajaxSetup({ 
         cache: true,
         contentType: "application/json; charset=utf-8",
         dataType: "jsonp"
     });
 
-    $.getJSON(url, function(response) {
-        $("#reason").html(getHTMLFrom(response))
+    $.getJSON(random(subreddit_url) + append, function(response) {
+        $("#reason").html(pickZoke(response))
         $("body").addClass("clickable")
     });
 
